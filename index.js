c91f16cefc1d89979c63945e3dbd6f6d7a2a9529
Sean Oyler <xrysen@gmail.com>
2020-11-02 00:41:23 +0000

    Solved

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
-}
+const sumItems = function(array) {
+  array = array.flat(Infinity);
+  if (array.length === 0) {
+    return 0;
+  } else {
+    return array[0] + sumItems(array.slice(1));
+
+  }
+};
 
 module.exports = sumItems;
